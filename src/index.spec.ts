3d6cedc5255113866d368e04024fc8daaf57b571
Owen Kelly <owen@owenkelly.com.au>
2018-02-14 20:31:18 +1100

    added some tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,30 @@
 import test from "ava";
+import { Wahn, Policy, PolicyAction } from "./index";
 
-const fn = async () => Promise.resolve("foo");
+// [ Simple Policy ]--------------------------------------------------------------------------------
+const simplePolicy: Policy = {
+    resources: ["test::resource"],
+    action: PolicyAction.Allow,
+};
 
-test(async t => {
-    t.is(await fn(), "foo");
+test("can create wahn class", async t => {
+    const wahn: Wahn = new Wahn({
+        policies: [simplePolicy],
+    });
+
+    // Check the instance is correct
+    t.true(wahn instanceof Wahn);
+
+    // Check we have policies
+    t.deepEqual(wahn.getPolicies(), [simplePolicy]);
 });
+
+test("evaluate simple policy", async t => {
+    const wahn: Wahn = new Wahn({
+        policies: [simplePolicy],
+    });
+});
+
+// [ Policy With Context ]--------------------------------------------------------------------------
+
+// [ Policy With User ]-----------------------------------------------------------------------------
--- src/index.ts
+++ src/index.ts
@@ -16,56 +16,85 @@
  *  condition: [
  *    {
  *      field: 'id',
- *      value: context.user.id,
+ *      value: 'context.user.id'
  *    }
  *  ]
  * }
  */
 
 class Wahn {
-  private policies: Policy[];
+    // Policies can be added at runtime only
+    private policies: Policy[];
 
-  constructor(options: WahnConstructorOptions) {
-    this.policies = options.policies;
-  }
+    constructor(options: WahnConstructorOptions) {
+        this.policies = options.policies;
+    }
+
+    /**
+     * Return all current policies
+     */
+    public getPolicies(): Policy[] {
+        return this.policies;
+    }
+
+    public evaluateAccess(): boolean | AuthorizationError {
+        return true;
+    }
 }
 
+// [ Types ]----------------------------------------------------------------------------------------
+
 type WahnConstructorOptions = {
-  policies: Policy[];
+    policies: Policy[];
+};
+
+type ContextUser = {
+    id: string;
+    ip?: string;
+    roles: string[];
 };
 
 type PolicyContext = {
-  [key: string]: any;
+    user?: ContextUser;
+    [key: string]: any;
 };
 
 enum PolicyAction {
-  allow,
-  deny
+    Allow,
+    Deny,
 }
 
 enum PolicyOperator {
-  is,
-  isNot
+    is,
+    isNot,
 }
 
 type PolicyCondition = {
-  field: string;
-  // A dot path to the value on the context object
-  value: string;
-  operator: PolicyOperator;
+    field: string;
+    // A dot path to the value on the context object
+    value: string;
+    operator: PolicyOperator;
 };
 
 type Policy = {
-  resources: string[];
-  action: PolicyAction;
-  condition: [PolicyCondition];
+    resources: string[];
+    action: PolicyAction;
+    condition?: [PolicyCondition];
+    roles?: string[];
 };
 
+// [ Errors ]---------------------------------------------------------------------------------------
+
+interface EvalError extends Error {}
+interface AuthorizationError extends Error {}
+
+// [ Export ]---------------------------------------------------------------------------------------
+
 export {
-  Wahn,
-  Policy,
-  PolicyAction,
-  PolicyCondition,
-  PolicyContext,
-  PolicyOperator
+    Wahn,
+    Policy,
+    PolicyAction,
+    PolicyCondition,
+    PolicyContext,
+    PolicyOperator,
 };
